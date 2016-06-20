c4e9bb60faf4ee0f4c55aef746ed699f5ed1cc05
aemoe <aemoe@icloud.com>
2016-06-20 17:54:25 +0800

    update gulpfile.js  for example of use with autoprefixer,postcss-modules and precss

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,19 @@ var postcss = require('gulp-postcss');
 
 gulp.task('default', function() {
     return gulp.src('src/**/*.css')
-        .pipe(postcss([require('postcss-display-inline-block')]))
+        .pipe(postcss([
+            require('precss'),
+        ]))
+        .pipe(postcss([
+            require('postcss-display-inline-block'),
+        ]))
+        .pipe(postcss([
+            require('autoprefixer'),
+        ]))
+        .pipe(postcss([
+            require('postcss-modules')({
+                generateScopedName: '[name]__[local]___[hash:base64:5]',
+            }),
+        ]))
         .pipe(gulp.dest('build/'));
 });
